56c3285baf4fc2953a180e3ef18ca49e958fad19
David Weid <david.weid.2@gmail.com>
2019-02-14 17:35:13 -0500

    inquirer validate

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,10 +14,12 @@ var userShop = function() {
             name: "product_id",
             message: "Please enter the product id of your desired item:",
             validate: function(input) {
-                if (typeof input !== Number) {
-                    console.log("You need to provide a number");
-                    return;
-                };
+
+                if (isNaN(input) === false && parseInt(input) > 0) {
+                    return true;
+                }
+                
+                return false;
             }
         }
 
