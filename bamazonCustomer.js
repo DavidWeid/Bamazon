06a81c8f556b8a2de11e4e19c87ec903eb638bac
David Weid <david.weid.2@gmail.com>
2019-02-13 23:16:47 -0500

    update start functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,32 @@
 var inquirer = require("inquirer");
 
-inquirer.prompt([
+var displayShop = function() {
 
-]).then(function(answer) {
+    // Display info from products table
 
-});
+};
+
+var userShop = function() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "product_id",
+            message: "Please enter the product id of your desired item:",
+            validate: function(input) {
+                if (typeof input !== Number) {
+                    console.log("You need to provide a number");
+                    return;
+                };
+            }
+        }
+
+    ]).then(function(answer) {
+
+        //Function
+
+    });
+
+};
+
+userShop();
